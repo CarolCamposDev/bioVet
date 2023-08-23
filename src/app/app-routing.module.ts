8ad4b22f40a049f24d1c28a1568232d07ed3f4bb
Carolina Campos <carolinarodriguesjf@gmail.com>
2023-08-23 20:59:16 -0300

    Adicionar componentes da página de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
+import { LoginComponent } from './login/login.component';
+import { BanhoETosaComponent } from './banho-e-tosa/banho-e-tosa.component';
+import { ProdutoComponent } from './produto/produto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'principal', 
+    component: PrincipalComponent
+  },
+
+  {
+    path: 'login', 
+    component: LoginComponent
+  },
+
+  {
+    path: 'banho-e-tosa',
+    component: BanhoETosaComponent
+  },
+
+  {
+    path: 'produto',
+    component: ProdutoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
